d4d82d111fd436945b776a0cd62f48df1e14776f
votrai <votrai123>
2019-11-28 15:57:04 +0700

    ban dau

--- routes/web.php
+++ routes/web.php
@@ -80,4 +80,3 @@ Route::group(['prefix' => 'admin', 'as'=>'admin'], function () {
         Route::post('delcategorys/{id}', 'Pages\\Auth\\AdminController\\AdminCategorys@postDelcategory');
     });
 });
-hkjsadahfkjs
